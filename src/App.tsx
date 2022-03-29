1534ad8f081d7d1020e55b057bda10b488166d4c
Dawid Razny <drazny@shoprunner.com>
2022-03-29 12:14:21 +0200

    Get Movies from mocked API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Movie } from './types';
 import MovieCard from './components/MovieCard';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const mockedURL = 'http://localhost:3004/movies';
 
-  const sampleMovie: Movie = {
-    id: '1and3011',
-    imageURL:
-      'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUzNTE2NTkzMV5BMl5BanBnXkFtZTgwMDAzOTUyMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
-    title: 'Inferno',
-    summary:
-      'Duis ut libero a nulla pharetra vulputate. Nullam congue ornare ligula bibendum blandit. Nullam nunc neque, viverra ut malesuada eu, placerat non lectus. Nam consectetur ligula ac dolor eleifend porta. ',
-    rating: 5.3,
-  };
+  useEffect(() => {
+    async function getMovies(){
+      const response = await fetch(mockedURL);
+      const data = await response.json();
+      setMovies(data);
+    }
+    getMovies();
+  }, [])
 
   return (
     <div className="App">
-      <MovieCard movieItem={sampleMovie} />
+      {movies && <MovieCard movieItem={movies[0]} /> }
     </div>
   );
 }

aa7873960f3fa19f479da055d4db967e61fc1b46
Dawid Razny <drazny@shoprunner.com>
2022-03-29 19:09:36 +0200

    Test Buttons and fix Movie Card

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import NotificationSnackBar from './components/NotificationSnackBar';
 import MovieCard from './components/MovieCard';
@@ -16,16 +16,16 @@ afterEach(() => {
   container = null;
 });
 
-describe('MovieCard', function () {
-  const fakeMovie = {
-    id: '1234',
-    imageURL:
-      'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUzNTE2NTkzMV5BMl5BanBnXkFtZTgwMDAzOTUyMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
-    title: 'Fake Movie',
-    summary: 'Nullam nunc neque, viverra ut malesuada eu, placerat non lectus.',
-    rating: 1.5,
-  };
+const fakeMovie = {
+  id: '1234',
+  imageURL:
+    'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUzNTE2NTkzMV5BMl5BanBnXkFtZTgwMDAzOTUyMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+  title: 'Fake Movie',
+  summary: 'Nullam nunc neque, viverra ut malesuada eu, placerat non lectus.',
+  rating: 1.5,
+};
 
+describe('MovieCard', function () {
   it('Render MovieCard properly', () => {
     render(
       <MovieCard
@@ -46,7 +46,41 @@ describe('MovieCard', function () {
     expect(movieRaiting).toBeInTheDocument();
     const movieIMG = screen.getByRole('img');
     expect(movieIMG).toBeInTheDocument();
+    const buttons = screen.getAllByRole('button')
+    expect(buttons.length).toEqual(2);
   });
+});
+
+describe('Action Buttons', function () {
+  test('Button Animation Accept', () => {
+      render(
+      <MovieCard 
+        key={fakeMovie.id}
+        movieItem={fakeMovie}
+        position={1}
+        disabled={false}
+        updateMovies={(action: string) => {}} />, container);
+  
+    fireEvent.click(screen.getByText('Accept'))
+  
+    const movieAnimationAccept = document.querySelector(".movie-card.movie-card--animate-accept")
+    expect(movieAnimationAccept).toBeInTheDocument();
+  })
+  
+  test('Button Animation Reject', () => {
+      render(
+      <MovieCard 
+        key={fakeMovie.id}
+        movieItem={fakeMovie}
+        position={1}
+        disabled={false}
+        updateMovies={(action: string) => {}} />, container);
+  
+    fireEvent.click(screen.getByText('Reject'))
+  
+    const movieAnimationReject = document.querySelector(".movie-card.movie-card--animate-reject")
+    expect(movieAnimationReject).toBeInTheDocument();
+  })
 });
 
 describe('SnackBar', function () {

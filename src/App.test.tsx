f41c6066aeb282ddbd603a65b99db0efb008a196
Dawid Razny <drazny@shoprunner.com>
2022-03-29 15:33:08 +0200

    Fixing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { act, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import NotificationSnackBar from './components/NotificationSnackBar';
-import App from './App';
 
 let container: any = null;
 beforeEach(() => {
-  container = document.createElement("div");
+  container = document.createElement('div');
   document.body.appendChild(container);
 });
 
@@ -15,14 +14,13 @@ afterEach(() => {
   container.remove();
   container = null;
 });
+
 it('SnackBar Sample Test', () => {
-  act(() => {
-    const snackBarSampleData = { open: true, action: 'accept' };
-    render(
-      <NotificationSnackBar snackBarOptions={snackBarSampleData} />,
-      container
-    );
-  });
+  const snackBarSampleData = { open: true, action: 'accept' };
+  render(
+    <NotificationSnackBar snackBarOptions={snackBarSampleData} />,
+    container
+  );
   const snackBarAccept = screen.getByText('You liked the movie!');
   expect(snackBarAccept).toBeInTheDocument();
 });

5c5362907677d93ef0b6651d67c3a69e118927d9
Dawid Razny <drazny@shoprunner.com>
2022-03-29 15:31:16 +0200

    sample SnackBar test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
+import NotificationSnackBar from './components/NotificationSnackBar';
 import App from './App';
 
-test('Random Test', () => {
-  render(<App />);
-  const linkElement = screen.findByRole("div");
-  expect(linkElement).toBeInTheDocument();
+let container: any = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+it('SnackBar Sample Test', () => {
+  act(() => {
+    const snackBarSampleData = { open: true, action: 'accept' };
+    render(
+      <NotificationSnackBar snackBarOptions={snackBarSampleData} />,
+      container
+    );
+  });
+  const snackBarAccept = screen.getByText('You liked the movie!');
+  expect(snackBarAccept).toBeInTheDocument();
 });

a57f68492d2efb74d2c66301a7e84c6d264ce843
Dawid Razny <drazny@shoprunner.com>
2022-03-29 15:18:07 +0200

    Add random test to test github actions

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('Random Test', () => {
+  render(<App />);
+  const linkElement = document.querySelector("div");
+  expect(linkElement).toBeInTheDocument();
+});
